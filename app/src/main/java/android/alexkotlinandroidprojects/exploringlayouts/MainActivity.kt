3aa3fba492d124110d6f38922d47b4cfdf77140a
Alexander Rau <alexanderrau86@yahoo.com>
2020-01-08 14:44:03 -0700

    Begin Chpater 4 exercises

--- app/src/main/java/android/alexkotlinandroidprojects/exploringlayouts/MainActivity.kt
+++ app/src/main/java/android/alexkotlinandroidprojects/exploringlayouts/MainActivity.kt
@@ -2,11 +2,21 @@ package android.alexkotlinandroidprojects.exploringlayouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        fun loadConstraintLayout(v: View) {
+            setContentView(R.layout.activity_main)
+        }
+
+        fun loadTableLayout(v: View) {
+//            setContentView(R.layout.my_table_layout)
+        }
+
+        setContentView(R.layout.main_menu)
     }
 }
