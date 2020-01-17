0865d1f052de3aecb1b1359649b1c0e5bd2fd426
Alexander Rau <alexanderrau86@yahoo.com>
2020-01-17 15:53:54 -0700

    continue chpater 4

--- app/src/main/java/android/alexkotlinandroidprojects/exploringlayouts/MainActivity.kt
+++ app/src/main/java/android/alexkotlinandroidprojects/exploringlayouts/MainActivity.kt
@@ -8,15 +8,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        fun loadConstraintLayout(v: View) {
-            setContentView(R.layout.activity_main)
-        }
-
-        fun loadTableLayout(v: View) {
-//            setContentView(R.layout.my_table_layout)
-        }
-
         setContentView(R.layout.main_menu)
+    }
+
+    fun loadConstraintLayout(v: View) {
+        setContentView(R.layout.activity_main)
+    }
+
+    fun loadTableLayout(v: View) {
+//            setContentView(R.layout.my_table_layout)
     }
 }
